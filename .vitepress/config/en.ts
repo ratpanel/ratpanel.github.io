86b8921565372a3080dc6cfdc7d019eb98c05de1
耗子 <haozi@loli.email>
2025-05-29 22:01:28 +0800

    fix: lint

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -31,7 +31,7 @@ export const config = defineConfig({
                     ...versions.map((version: string) => {
                         return {
                             text: version,
-                            link: `/version-${version}`
+                            link: "/version-" + version
                         }
                     })
                 ]
